7560bcb51564164821ead3b5289cc40e685bc81c
Dima Kuzmich <dima.kuzmich.work@gmail.com>
2016-10-08 14:48:23 +0300

    add Controls component and tests

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
       Countdown: 'app/components/Countdown.jsx',
       CountdownForm: 'app/components/CountdownForm.jsx',
       Clock: 'app/components/Clock.jsx',
+      Controls: 'app/components/Controls.jsx',
       aplicationStyles: 'app/style/app.scss'
     },
     extensions: ['', '.js', '.jsx']
