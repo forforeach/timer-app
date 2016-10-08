8f32b7b7150b8c5848bbb63571069c59fd662921
Dima Kuzmich <dima.kuzmich.work@gmail.com>
2016-10-08 12:43:21 +0300

    add clock implementation and tests

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,11 @@
 const React = require('react');
+const Clock = require('Clock');
 
 var Countdown = React.createClass({
   render: function() {
     return (
       <div>
-        <p>Countdown</p>
+        <Clock totalSeconds={129}/>
       </div>
     );
   }
